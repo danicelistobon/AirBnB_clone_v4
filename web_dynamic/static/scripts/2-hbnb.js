af9580d1ddbbc79bc5249be9b6bc6077d60c1524
danicelistobon <danielct-97@hotmail.com>
2020-02-17 18:23:33 +0000

    3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,41 @@
+const amens = [];
+
+function addAmensHtml (amens) {
+  $('DIV.amenities h4').text(amens.join(', '));
+}
+
+function addAmens (name) {
+  amens.push(name);
+}
+
+function deleteAmens (name) {
+  for (let i = 0; i < amens.length; i++) {
+    if (amens[i] === name) {
+      amens.splice(i, 1);
+    }
+  }
+}
+
+function init () {
+  $('DIV.amenities .popover LI input').change(function () {
+    const name = $(this).data('name');
+
+    if (this.checked) {
+      addAmens(name);
+    } else {
+      deleteAmens(name);
+    }
+
+    addAmensHtml(amens);
+  });
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, data => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+}
+
+$(document).ready(init);
